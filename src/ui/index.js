e4ada6d56ca3328d656c115c18cd4978731d10a5
CiroLee <ciromail@126.com>
2025-10-09 21:57:59 +0800

    chore: update ignore

--- src/ui/index.js
+++
@@ -1,6 +0,0 @@
-// 自动注册所有Web Components
-// 本文件由 build-components.js 自动生成，请勿手动编辑
-import './button/index.js';
-import './modal/index.js';
-import './tag/index.js';
-import './theme-provider/index.js';
