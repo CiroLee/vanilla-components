fd00ef705ec2c301041ce01bc4cfac59552343e9
CiroLee <ciromail@126.com>
2025-10-13 23:24:20 +0800

    feat: 增加va-switch组件

---
+++ src/docs/codes/switch.js
@@ -0,0 +1,6 @@
+export const sizeCode = `<va-switch size="sm" id="sm">Small</va-switch>
+<va-switch size="md" id="md">Medium</va-switch>
+<va-switch size="lg" id="lg">Large</va-switch>`;
+
+export const disabledCode = `<va-switch id="sw-1" disabled>Disabled</va-switch>
+<va-switch id="sw-2" disabled checked>Disabled</va-switch>`;
