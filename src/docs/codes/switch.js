275ac920ea4bd281e63764231179765f02fbfc94
CiroLee <ciromail@126.com>
2025-10-14 13:53:58 +0800

    feat: 优化api表格

--- src/docs/codes/switch.js
+++ src/docs/codes/switch.js
@@ -4,3 +4,9 @@ export const sizeCode = `<va-switch size="sm" id="sm">Small</va-switch>
 
 export const disabledCode = `<va-switch id="sw-1" disabled>Disabled</va-switch>
 <va-switch id="sw-2" disabled checked>Disabled</va-switch>`;
+export const labelCode = `<va-switch id="switch-auto" class="gap">Auto Translate</va-switch>`;
+export const labelCssCode = `
+/* CSS */
+.gap {
+  gap: 4px;
+}`;
