45b42ba40496c34425616a4d1cc69d1e28e8d953
CiroLee <ciromail@126.com>
2025-12-30 13:34:48 +0800

    feat: va-tooltip组件

---
+++ src/docs/codes/tooltip.js
@@ -0,0 +1,26 @@
+export const defaultCode = `<va-tooltip content="This is a tooltip">
+  <va-action>Hover me</va-action>
+</va-tooltip>`;
+
+export const placementCode = `<div class="flex gap-4">
+  <va-tooltip content="This is a tooltip" placement="top">
+    <va-action>top</va-action>
+  </va-tooltip>
+  <va-tooltip content="This is a tooltip" placement="right">
+    <va-action>right</va-action>
+  </va-tooltip>
+  <va-tooltip content="This is a tooltip" placement="bottom">
+    <va-action>bottom</va-action>
+  </va-tooltip>
+  <va-tooltip content="This is a tooltip" placement="left">
+    <va-action>left</va-action>
+  </va-tooltip>
+</div>`;
+
+export const delayCode = `<va-tooltip content="This is a tooltip" delay="1000">
+  <va-action>Hover me</va-action>
+</va-tooltip>`;
+
+export const offsetCode = `<va-tooltip content="This is a tooltip" offset="12">
+  <va-action>Hover me</va-action>
+</va-tooltip>`;
